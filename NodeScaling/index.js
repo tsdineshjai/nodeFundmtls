2763dc02e4509a672ca147c361a44bc050ca17b3
tanishka <tsdineshjai@gmail.com>
2023-08-18 15:57:21 +0530

    watcher over a file changes was implemented

--- NodeScaling/index.js
+++ NodeScaling/index.js
@@ -1,5 +1,24 @@
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
+(async () => {
+	const fileHandle = await fs.open("./watch.txt", "r");
+	//Opens a FileHandle and a number(id) will be assigned to it for tracking process
 
-// const content = fs.readFileSync(__dirname + "wishes.text");
+	fileHandle.on("change", async () => {
+		//callback is getting executed once the change event is identified.
+		const bufferSize = await fileHandle.stat();
+		const { size } = bufferSize;
+		const contents = await fileHandle.read(Buffer.alloc(size), 0, size, 0);
+		console.log(contents.buffer.toString());
+	});
 
-console.log(__filename);
+	//watcher...
+	const watcher = fs.watch("./watch.txt");
+	for await (const event of watcher) {
+		console.log(event);
+		/* event --> { eventType: 'change', filename: 'watch.txt' }	*/
+		if (event.eventType === "change") {
+			fileHandle.emit("change");
+			//emitting a "change" event.
+		}
+	}
+})();
