1f668db1c98d293bff61bf4dc5db48bc7860ecc8
tanishka <tsdineshjai@gmail.com>
2023-11-28 16:33:22 +0530

    event module refresher

--- NodeScaling/AsyncLibUv.js
+++ NodeScaling/AsyncLibUv.js
@@ -19,3 +19,19 @@ setTimeout(() => {
 //and then the timerMethods
 
 /* among the timer callbacks, priority is for order of functions exeucted. */
+
+setImmediate(() => {
+	console.log(`setImmediate are the callbacks part of the check queue and it is in priority
+  after the microtTasks, timerqueu, I/O`);
+});
+//the last one to get called
+
+process.nextTick(() => {
+	console.log(`i get the first priority in microTasks queue `);
+});
+//this gets excuted first.
+
+setTimeout(() => {
+	console.log(`after the microTask queues, timer CB's gets the next priority`);
+}, 0);
+//and then the timerMethods
