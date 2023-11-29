8435349842971a27d7b854decd4d56e68eabae3d
tanishka <tsdineshjai@gmail.com>
2023-11-29 12:49:08 +0530

    stream draining

--- NodeScaling/streams/writeMany.js
+++ NodeScaling/streams/writeMany.js
@@ -30,31 +30,33 @@
 /* this is call back API, always there is an associated CB which will be called once the async 
 operations is performed.
 */
-const fs = require("node:fs");
-
-(async () => {
-	console.time("started");
-
-	fs.open("./test.txt", "w", (err, fd) => {
-		for (let i = 0; i < 10000; i++) {
-			const buff = Buffer.from(`${i}`, "utf-8");
-			fs.write(fd, buff, () => {});
-		}
-	});
-	console.timeEnd("started");
-})();
-
-// const fs = require("node:fs/promises");
+// const fs = require("node:fs");
 
 // (async () => {
 // 	console.time("started");
 
-// 	const fileHandle = await fs.open("./test.txt", "w");
-// 	const stream = fileHandle.createWriteStream();
-// 	for (let i = 0; i < 10000; i++) {
-// 		const buff = Buffer.from(`${i}`, "utf-8");
-// 		stream.write(buff);
-// 	}
-
+// 	fs.open("./test.txt", "w", (err, fd) => {
+// 		for (let i = 0; i < 10000; i++) {
+// 			const buff = Buffer.from(`${i}`, "utf-8");
+// 			fs.write(fd, buff, () => {});
+// 		}
+// 	});
 // 	console.timeEnd("started");
 // })();
+
+const fs = require("node:fs/promises");
+
+(async () => {
+	const fileHandle = await fs.open("./test.txt", "w");
+	const stream = fileHandle.createWriteStream();
+	console.log(stream._writableState.highWaterMark);
+	// for (let i = 0; i < 10000; i++) {
+	// 	const buff = Buffer.from(`${i}`, "utf-8");
+	// 	stream.write(buff);
+	// }
+	const buff1 = Buffer.alloc(16381, "a");
+	stream.write(buff1);
+	stream.on("drain", () => {
+		console.log(`we need to drain to further process`);
+	});
+})();
