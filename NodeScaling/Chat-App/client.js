cc6032419fb7ac7a70446dd2e650c97ee9b4496e
tanishka <tsdineshjai@gmail.com>
2023-12-07 12:29:47 +0530

    multi users connecting to the server feature

--- NodeScaling/Chat-App/client.js
+++ NodeScaling/Chat-App/client.js
@@ -1,7 +1,30 @@
 const net = require("node:net");
 
-const client = net.createConnection({ port: 3542, host: "127.0.0.1" }, () => {
-	console.log(`connected to the server`);
+const readline = require("node:readline");
+
+/* 
+createConnection for IPC connections. Inter Process Communications !
+createConnection for TCP connections.
+*/
+//establishing a connection.
+const client = net.createConnection(
+	{ port: 3542, host: "127.0.0.1" },
+	(socket) => {
+		console.log(`connected to the server`);
+	}
+);
+
+//The readline.createInterface() method creates a new readline.Interfaceinstance.
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
 });
 
-client.write("hello");
+//asking a question using the readline question method
+//signal is passed to abort the question when we dont get a  response within some time
+//this is where you can pose questions and wait for answer and callback listens for answer and logs its
+rl.question("Enter a message to be sent to client\n ", (answer) => {
+	client.write(answer);
+});
+
+
--- NodeScaling/Chat-App/server.js
+++ NodeScaling/Chat-App/server.js
@@ -5,12 +5,17 @@ const net = require("node:net");
 const server = net.createServer((socket) => {
 	//this listener automatically listens to the connection event and emits the below log
 	console.log(`client has joined to the server`);
-
-	socket.on("data", (data) => {
-		console.log(`Received data from client: ${data}`);
+	socket.on("data", (message) => {
+		console.log(`message was received : ${message}`);
 	});
 });
 
+server.on("connection", (socket) => {
+	//socket we get here is the client socket that has connected to the server
+	//we have initiated client connection in the client.js file using net.createConnection() method
+	console.log(`client has connected to the server`);
+});
+
 server.listen(3542, "127.0.0.1", () => {
-	console.log(`server is up and lisetening`);
+	console.log(`server is up and lisetening`, server.address());
 });
