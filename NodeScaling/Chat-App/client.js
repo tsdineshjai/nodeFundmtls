847336560e5d837995a3fedf6e14521c69a5fb23
tanishka <tsdineshjai@gmail.com>
2023-12-07 15:23:33 +0530

    terminal feature of clearing out the prompt and entering the new message

--- NodeScaling/Chat-App/client.js
+++ NodeScaling/Chat-App/client.js
@@ -2,29 +2,28 @@ const net = require("node:net");
 
 const readline = require("node:readline");
 
-/* 
-createConnection for IPC connections. Inter Process Communications !
-createConnection for TCP connections.
-*/
-//establishing a connection.
-const client = net.createConnection(
-	{ port: 3542, host: "127.0.0.1" },
-	(socket) => {
-		console.log(`connected to the server`);
-	}
-);
-
-//The readline.createInterface() method creates a new readline.Interfaceinstance.
+//The readline.createInterface() method creates a new readline.Interface instance.
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 });
 
-//asking a question using the readline question method
-//signal is passed to abort the question when we dont get a  response within some time
-//this is where you can pose questions and wait for answer and callback listens for answer and logs its
-rl.question("Enter a message to be sent to client\n ", (answer) => {
-	client.write(answer);
+const client = net.createConnection(
+	{ port: 3542, host: "127.0.0.1" },
+	() => {}
+);
+
+const ask = () => {
+	rl.question("Enter a message > ", (answer) => {
+		process.stdout.moveCursor(0, -1, () => {});
+		process.stdout.clearLine(0, () => {});
+		client.write(answer);
+	});
+};
+
+ask();
+
+client.on("data", (message) => {
+	console.log(message.toString("utf-8"));
+	ask();
 });
-
-
--- NodeScaling/Chat-App/server.js
+++ NodeScaling/Chat-App/server.js
@@ -2,18 +2,23 @@ const net = require("node:net");
 
 //create a server using createServer Method
 
-const server = net.createServer((socket) => {
-	//this listener automatically listens to the connection event and emits the below log
-	console.log(`client has joined to the server`);
-	socket.on("data", (message) => {
-		console.log(`message was received : ${message}`);
-	});
-});
-
+//we are creating a variable to store all the clients so that we can send the message all the clients if
+//some client does the message,
+//Server is chat-room, all clients connected should see each other messages when they ping in the chat room
+const clientsArr = [];
+const server = net.createServer();
 server.on("connection", (socket) => {
-	//socket we get here is the client socket that has connected to the server
-	//we have initiated client connection in the client.js file using net.createConnection() method
+	//socket is a client object, its a duplex stream
 	console.log(`client has connected to the server`);
+	//this listener automatically listens to the connection event and invokes this callback
+	// everytime a client joins the server
+	clientsArr.push(socket);
+	console.log(clientsArr.length);
+	socket.on("data", (message) => {
+		clientsArr.map((c) => {
+			c.write(message);
+		});
+	});
 });
 
 server.listen(3542, "127.0.0.1", () => {
